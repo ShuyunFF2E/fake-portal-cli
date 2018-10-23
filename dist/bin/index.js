2b91b32b43247fec2e37d27d2942fc9d9954083f
baukh789 <182209508@qq.com>
2018-10-23 10:33:04 +0800

    删除无用文件

--- dist/bin/index.js
+++
@@ -1,8 +0,0 @@
-const program = require("commander");
-const { version } = require("../package");
-
-program
-.version('0.0.1')
-.usage('<command> [options]')
-.command('init <project-name>', '初始化 fake portal')
-.parse(process.argv);
